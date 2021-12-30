09527c2b18f122ebf7348a47844b086f0fb3d579
Danish Kamal <danish5095@gmail.com>
2021-12-30 19:05:07 +0530

    Initial Commit

---
+++ mongoProject/kill.sh
@@ -0,0 +1,4 @@
+kubectl delete -f mongo-secret.yml
+kubectl delete -f mongo-config.yml
+kubectl delete -f mongo-express.yml
+kubectl delete -f mongo.yml
---
+++ mongoProject/run.sh
@@ -0,0 +1,5 @@
+kubectl apply -f mongo-secret.yml
+kubectl apply -f mongo-config.yml
+kubectl apply -f mongo.yml
+kubectl apply -f mongo-express.yml
+minikube service mongo-express-service
